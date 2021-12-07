8b161bc3f87dabece515e4094c2dc0e9340a03a1
ianet <ianet@users.noreply.github.com>
2021-12-06 22:50:35 -1000

    init

--- testonnxruntime.jl
+++ testonnxruntime.jl
@@ -8,6 +8,7 @@ ortapi = unsafe_load(portapi)
 rpenv = Ref{Ptr{ORT.OrtEnv}}(0)
 status = @ccall $(ortapi.CreateEnv)(ORT.ORT_LOGGING_LEVEL_WARNING::Cint, "Test"::Cstring, rpenv::Ref{Ptr{ORT.OrtEnv}})::Ptr{ORT.OrtStatus}
 @show status
+
 if status != Ptr{ORT.OrtStatus}(0)
     msg = (@ccall $(ortapi.GetErrorMessage)(status::Ptr{ORT.OrtStatus})::Cstring) |> unsafe_string
     code = @ccall $(ortapi.GetErrorCode)(status::Ptr{ORT.OrtStatus})::Cint
@@ -18,6 +19,4 @@ rpsession_options = Ref(Ptr{ORT.OrtSessionOptions}(0))
 status = @ccall $(ortapi.CreateSessionOptions)(rpsession_options::Ref{Ptr{ORT.OrtSessionOptions}})::Ptr{ORT.OrtStatus}
 @show status
 
-
-
-
+# TBD
