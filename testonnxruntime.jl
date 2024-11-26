f0ff7b3323987764f56552d155b0cfde5bab4c88
Ian Ellison-Taylor <ianet@microsoft.com>
2024-11-26 15:00:18 -0800

    ...

--- testonnxruntime.jl
+++ testonnxruntime.jl
@@ -1,22 +1,22 @@
-import LibOnnxRuntime as ORT
+using LibOnnxRuntime
 
-papibase = ORT.OrtGetApiBase()
+papibase = OrtGetApiBase()
 apibase = unsafe_load(papibase)
-portapi = @ccall $(apibase.GetApi)(ORT.ORT_API_VERSION::UInt32)::Ptr{ORT.OrtApi}
+portapi = @ccall $(apibase.GetApi)(ORT_API_VERSION::UInt32)::Ptr{OrtApi}
 ortapi = unsafe_load(portapi)
 
-rpenv = Ref{Ptr{ORT.OrtEnv}}(0)
-status = @ccall $(ortapi.CreateEnv)(ORT.ORT_LOGGING_LEVEL_WARNING::Cint, "Test"::Cstring, rpenv::Ref{Ptr{ORT.OrtEnv}})::Ptr{ORT.OrtStatus}
-@show status
+rpenv = Ref{Ptr{OrtEnv}}(0)
+status = @ccall $(ortapi.CreateEnv)(ORT_LOGGING_LEVEL_VERBOSE::Cint, "Test"::Cstring, rpenv::Ref{Ptr{OrtEnv}})::Ptr{OrtStatus}
+@info "CreateEnv" status rpenv[]
 
-if status != Ptr{ORT.OrtStatus}(0)
-    msg = (@ccall $(ortapi.GetErrorMessage)(status::Ptr{ORT.OrtStatus})::Cstring) |> unsafe_string
-    code = @ccall $(ortapi.GetErrorCode)(status::Ptr{ORT.OrtStatus})::Cint
+if status != Ptr{OrtStatus}(0)
+    msg = (@ccall $(ortapi.GetErrorMessage)(status::Ptr{OrtStatus})::Cstring) |> unsafe_string
+    code = @ccall $(ortapi.GetErrorCode)(status::Ptr{OrtStatus})::Cint
     println("Status: $code $msg")
 end
 
-rpsession_options = Ref(Ptr{ORT.OrtSessionOptions}(0))
-status = @ccall $(ortapi.CreateSessionOptions)(rpsession_options::Ref{Ptr{ORT.OrtSessionOptions}})::Ptr{ORT.OrtStatus}
-@show status
+rpsession_options = Ref(Ptr{OrtSessionOptions}(0))
+status = @ccall $(ortapi.CreateSessionOptions)(rpsession_options::Ref{Ptr{OrtSessionOptions}})::Ptr{OrtStatus}
+@info "CreateSessionOptions" status rpsession_options[]
 
 # TBD
