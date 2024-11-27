cdb930f7649cfba0d7915b2f3bf9aa773cf61693
4thjuly <july4th@ellison-taylor.us>
2024-11-26 21:26:13 -0800

    ...

--- testonnxruntime.jl
+++ testonnxruntime.jl
@@ -1,21 +1,25 @@
 using LibOnnxRuntime
 
+GetApi(base, version) = (@ccall $(base.GetApi)(version::UInt32)::Ptr{OrtApi}) |> unsafe_load
+CreateEnv(ort, level, status, env) = @ccall $(ort.CreateEnv)(level::Cint, status::Cstring, env::Ptr{Ptr{OrtEnv}})::OrtStatusPtr
+GetErrorMessage(ort, status) = (@ccall $(ort.GetErrorMessage)(status::OrtStatusPtr)::Cstring) |> unsafe_string
+GetErrorCode(ort, status) = @ccall $(ort.GetErrorCode)(status::OrtStatusPtr)::Cint
+CreateSessionOptions(ort, options) = @ccall $(ort.CreateSessionOptions)(options::Ptr{Ptr{OrtSessionOptions}})::OrtStatusPtr
+
 base = OrtGetApiBase() |> unsafe_load
-ort = (@ccall $(base.GetApi)(ORT_API_VERSION::UInt32)::Ptr{OrtApi}) |> unsafe_load
-
+ort = GetApi(base, ORT_API_VERSION)
 env = Ptr{OrtEnv}(0) |> Ref
-status = @ccall $(ort.CreateEnv)(ORT_LOGGING_LEVEL_VERBOSE::Cint, "Test"::Cstring, env::Ptr{Ptr{OrtEnv}})::OrtStatusPtr
-
+status = CreateEnv(ort, ORT_LOGGING_LEVEL_VERBOSE, "Test", env)
 @info "CreateEnv" status env[]
 
 if status != OrtStatusPtr(0)
-    msg = (@ccall $(ort.GetErrorMessage)(status::OrtStatusPtr)::Cstring) |> unsafe_string
-    code = @ccall $(ort.GetErrorCode)(status::OrtStatusPtr)::Cint
+    msg = GetErrorMessage(ort, status)
+    code = GetErrorCode(ort, status)
     println("Status: $code $msg")
 end
 
 options = Ptr{OrtSessionOptions}(0) |> Ref
-status = @ccall $(ort.CreateSessionOptions)(options::Ptr{Ptr{OrtSessionOptions}})::OrtStatusPtr
+status = CreateSessionOptions(ort, options)
 @info "CreateSessionOptions" status options[]
 
 # TBD
