5b4956c6ae90ab1d2d4efa275f134b187cae6794
Ian Ellison-Taylor <ianet@microsoft.com>
2024-11-27 15:19:06 -0800

    wip

--- testonnxruntime.jl
+++ testonnxruntime.jl
@@ -6,9 +6,14 @@ GetErrorMessage(ort, status) = (@ccall $(ort.GetErrorMessage)(status::OrtStatusP
 GetErrorCode(ort, status) = @ccall $(ort.GetErrorCode)(status::OrtStatusPtr)::Cint
 CreateSessionOptions(ort, options) = @ccall $(ort.CreateSessionOptions)(options::Ptr{Ptr{OrtSessionOptions}})::OrtStatusPtr
 
+# CreateSession(ort, env, model_path, options, session) = @ccall $(ort.CreateSession)(env::Ptr{OrtEnv}, model_path::Cstring, options::Ptr{OrtSessionOptions}, session::Ptr{Ptr{OrtSession}})::OrtStatusPtr
+# CreateMemoryInfo(ort, name, type, id, mem_type, memory_info) = @ccall $(ort.CreateMemoryInfo)(name::Cstring, type::OrtAllocatorType, id::Cint, mem_type::OrtMemType, memory_info::Ptr{Ptr{OrtMemoryInfo}})::OrtStatusPtr
+# Run(session, run_options, input_names, inputs, input_len, output_names, output_names_len, outputs) = @ccall $(ort.Run)()::OrtStatusPtr
+
+
 base = OrtGetApiBase() |> unsafe_load
 ort = GetApi(base, ORT_API_VERSION)
-env = Ptr{OrtEnv}(0) |> Ref
+env = Ptr{OrtEnv}(C_NULL) |> Ref
 status = CreateEnv(ort, ORT_LOGGING_LEVEL_VERBOSE, "Test", env)
 @info "CreateEnv" status env[]
 
@@ -21,5 +26,9 @@ end
 options = Ptr{OrtSessionOptions}(0) |> Ref
 status = CreateSessionOptions(ort, options)
 @info "CreateSessionOptions" status options[]
+session = Ptr{OrtSession}(C_NULL) |> Ref
 
 # TBD
+
+# status = CreateSession(ort, env[], "./path_to_model", options[], session)
+# status = Run(session, run_options, input_names, inputs, input_len, output_names, output_names_len, outputs)
